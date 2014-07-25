b4bade4550c8b12c526938a6736686f050673d6c
Takashi Matsuda <takashi@ddhcp218216.d.aist.go.jp>
2014-07-25 14:03:01 +0900

    add scoring script

--- testdata/dataset/scoring.rb
+++ testdata/dataset/scoring.rb
@@ -1,8 +1,9 @@
 #! /usr/bin/ruby
+# 一致する塩基対の数を表示するスクリプト.
 
 def secondstrc(f)
 	c = 0
-	res = ""
+	res = []
 	while l = f.gets
 		if (c % 3 == 2)
 			res << l
@@ -12,10 +13,49 @@ def secondstrc(f)
 	return res
 end
 
+#strc: RNA second structure (string)
+def innerbp(strc)
+	bp_stack = []
+	bp_list = []
+
+	len = strc.length
+	for i in 0..(len-1)
+		for j in 0..(strc[i].size-1)
+			if strc[i].eql?("(")
+				bp_stack << i
+			elsif strc[i].eql?(")")
+				bp_list << [bp_stack.delete_at(-1) , i]
+			end
+		end
+	end
+
+	return bp_list
+end
+
+
+def hybridbp(strc)
+	nandemo = []
+	bp_list = []
+
+	for i in 0..(strc[0].size - 1)
+		if strc[0][i] == "["
+			nandemo << i
+		end
+	end
+	for i in 0..(strc[1].size - 1)
+		if strc[1][i] == "]"
+			bp_list << [nandemo.delete_at(0), i]
+		end
+	end
+
+	print bp_list
+
+	return bp_list
+end
+
 files = Dir::entries(Dir::pwd)
 tcount = 0
 ncount = 0
-tot = 0
 for filepath in files
 	if /answer.fa$/ =~ filepath
 		puts "File Open: #{filepath}"
@@ -27,21 +67,30 @@ for filepath in files
 		test_strc = secondstrc(test_file)
 		puts ans_strc
 		puts test_strc
-		len= ans_strc.length
-		tot = tot + len
+
+		innerbp_ans_l = Object.new
+		innerbp_test_l = Object.new
+		innerbp_ans_l = innerbp(ans_strc)
+		innerbp_test_l = innerbp(test_strc)
+
+		hybrid_ans_l = Object.new
+		hybrid_test_l = Object.new
+		hybrid_ans_l = hybridbp(ans_strc)
+		hybrid_test_l = hybridbp(test_strc)
+
 		tc_tmp = 0
 		nc_tmp = 0
-		for i in 0..(len-1)
-			if ans_strc[i] == test_strc[i] then
-				tc_tmp = tc_tmp + 1
-			else
-				nc_tmp = nc_tmp + 1
-			end
+
+		
+		for i in 0..(innerbp_ans_l.size - 1)
+
+			tc_tmp = tc_tmp + innerbp_test_l.count(innerbp_ans_l[i])
+			tc_tmp = tc_tmp + hybrid_test_l.count(hybrid_ans_l[i])
 		end
 		tcount = tcount + tc_tmp
-		ncount = ncount + nc_tmp
+		#ncount = ncount + nc_tmp
 		puts "True : #{tc_tmp}"
-		puts "False: #{nc_tmp}"
+		#puts "False: #{nc_tmp}"
 	end
 end
 
