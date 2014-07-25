32d24f0de1b01c74863a292ef3ee2df78d666f3f
Takashi Matsuda <takashi@ddhcp218216.d.aist.go.jp>
2014-07-25 14:03:28 +0900

    add scoring script

---
+++ testdata/dataset/toysample/test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+for i in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
+do
+	/Users/takashi/cbrc/ractip_hom/src/ractip_hom -a $i gca_bovine.fasta sample.fasta gca_bovine2.fasta sample-2.fasta
+done
+
