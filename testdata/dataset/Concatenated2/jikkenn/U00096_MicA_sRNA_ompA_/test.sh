132e703391c096c7c2ca84da7dc5f131b95a1992
Takashi Matsuda <takashi@matsudatakashi-no-MacBook-Air.local>
2014-10-08 18:35:24 +0900

    air

--- testdata/dataset/Concatenated2/jikkenn/U00096_MicA_sRNA_ompA_/test.sh
+++ testdata/dataset/Concatenated2/jikkenn/U00096_MicA_sRNA_ompA_/test.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-for i in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+for i in 0.05 0.1 0.15 0.2 
 do
-	echo "mix_weight == ${i}"
-	/Users/takashi/cbrc/ractip_hom/src/ractip_hom --mix-weight=${i} -t 0.5 AE006468_1_1.fa AE006468_CyaR_sRNA_ompX_1.fa AE006468_1_2.fa AE006468_CyaR_sRNA_ompX_2.fa > "res/mix_w/mix_w_${i}.fa"
-	# 最初の_を用いて切断して、第0成分+_1_1, 2の配列に対してractip_homを行う。
-	# これRubyで書きなおした方が楽かも。
+    for j in 0.05 0.1 0.12 0.14 0.16
+    do
+	echo "ib-t == ${i}"
+	echo "ob-t == ${j}"
+	time ${RACTIP_HOM}/src/ractip_hom -t ${i} -u ${j} MicA.fa easy1.fa ompA.fa easy2.fa > newres/ibt-${i}-obt-${j}.fa
+    done
 done
 
---
+++ testdata/dataset/toysample/bp-matrix-wh-1/plotting.py
@@ -0,0 +1,52 @@
+#! /usr/bin/python
+# -*: coding:utf-8 -*-
+
+import scipy.interpolate
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+"""
+f = open('./out_hp_2.csv')
+buffers = f.readlines()
+mat = []
+for tmp in buffers:
+	mat.append(tmp.split(','))
+
+numat = []## must change to array
+for col in mat:
+	numat.append(map(lambda x: float(x), col))
+
+## interpolation
+
+## plotting
+
+fig = plt.figure()
+ax = Axes3D(fig)
+y = [i for i in range(len(numat))]
+x = [i for i in range(len(numat[0]))]
+X, Y = np.meshgrid(x,y)
+# 行列ではなく点の集合としてプロットするのがscatter
+"""
+#ax.scatter(X, Y, numat)
+#plt.show()
+"""
+#R = np.sqrt(X**2 + Y**2)
+Z = np.array(numat)
+print X
+print Y
+print Z
+"""
+data = np.loadtxt("./out_hp_2.csv", delimiter=',')
+
+x = np.arange(0, len(data), 1)
+y = np.arange(0, len(data[0]), 1)
+X, Y = np.meshgrid(x,y)
+
+print X, Y, data
+
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.plot_surface(X,Y,data)
+
+plt.show()
+
